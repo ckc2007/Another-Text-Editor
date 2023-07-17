bf112adeb67cefe8595ae498da62da150a2dd036
ckc2007 <ckc2007@gmail.com>
2023-07-17 16:57:04 -0400

    fixed typo in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,7 +6,7 @@ window.addEventListener("beforeinstallprompt", (event) => {
   //  The event object contains information about the installation prompt. The code sets window.deferredPrompt to the event object, which allows us to trigger the installation prompt later.
   window.deferredPrompt = event;
   // This line removes the "hidden" class from the butInstall element, making it visible. The "hidden" class is typically used to hide the install button initially, and it will be shown only when the PWA is installable.
-  butInstall.classList.toggle("hidded", false);
+  butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
